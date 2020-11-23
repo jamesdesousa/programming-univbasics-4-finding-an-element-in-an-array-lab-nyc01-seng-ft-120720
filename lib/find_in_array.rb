dcda6dde10bc30cb143628e5c80f28a7dec94347
James DeSousa <james.desousa45@gmail.com>
2020-11-23 22:12:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do 
-    if array[count] == value_to_find do 
+    if array[count] == value_to_find  
       return count 
     else 
       count = count + 1 
